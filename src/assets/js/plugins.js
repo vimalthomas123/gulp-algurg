0242e9e3b9d1ad5170327af006aec1f4ae370888
vimalthomas123 <108937145+vimalthomas123@users.noreply.github.com>
2022-07-08 18:33:28 +0530

    init

---
+++ src/assets/js/plugins.js
@@ -0,0 +1,14 @@
+//=require plugins/bootstrap.min.js
+//=require plugins/slick.min.js
+//=require plugins/swiper-bundle.js
+//=require plugins/jquery.validate.js
+//=require plugins/validate-methods.js
+//=require plugins/select2.min.js
+//=require plugins/jquery.matchHeight.js
+//=require plugins/jquery.mCustomScrollbar.min.js
+//=require plugins/jquery.fancybox.js
+
+// 3 plugins removed
+// 1. jquery.mask.js
+// 2. moment.js
+// 3. datepicker.js
